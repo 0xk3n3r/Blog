8b84036f95678aed18dec872d6ab445241b81264
k3n3r <Baolan.Shi@colorado.edu>
2025-10-26 01:13:31 +0200

    async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,25 +9,21 @@ morgan.token('body', (req) => {
   return ''
 })
 blogRouter.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-blogRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogRouter.get('/', async(request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch((error) => next(error))
+blogRouter.get('/:id', async(request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 })
 
-blogRouter.put('/:id', (request, response, next) => {
+blogRouter.put('/:id', async(request, response, next) => {
   const body = request.body
 
   const blog = {
@@ -37,49 +33,55 @@ blogRouter.put('/:id', (request, response, next) => {
     likes: body.likes
   }
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(error => next(error))
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndDelete(request.params.id)
-  .then(result => {
-      if (result) {
-        response.status(204).end()
-      } else {
-        response.status(404).json({ error: 'Blog not found' })
-      }
-    })
-    .catch(error => next(error))
+blogRouter.delete('/:id', async(request, response, next) => {
+  try {
+    const result = await Blog.findByIdAndDelete(request.params.id)
+    if (result) {
+      response.status(204).end()
+    } else {
+      response.status(404).json({ error: 'Blog not found' })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
-
+/*  Blog.findByIdAndDelete(request.params.id)
 const generateId = () => {
   const maxId = blogs.length > 0
     ? Math.max(...blogs.map(n => n.id))
     : 0
   return maxId + 1
 }
+*/
 
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async(request, response, next) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogRouter.get('/info', (request, response) => {
+blogRouter.get('/info', async(request, response) => {
   //const maxId = blogs.length
   //response.send(`Phonebook has info for ${maxId} people<br> ${new Date()}`)
-  Blog.countDocuments({})
-    .then(count => {
-      response.send(`Phonebook has info for ${count} people<br> ${new Date()}`)
-    })
+  try {
+    const count = await Blog.countDocuments({})
+    response.send(`Phonebook has info for ${count} people<br> ${new Date()}`)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = blogRouter
